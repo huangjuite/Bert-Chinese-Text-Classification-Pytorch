002875acccfb6b1c333c3ca4a1463120d4a01202
huangjuite <huangjuite@gmail.com>
2020-06-20 15:44:02 +0800

    dl class

--- models/bert_RNN.py
+++ models/bert_RNN.py
@@ -3,6 +3,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from pytorch_pretrained import BertModel, BertTokenizer
+import os
+
 
 
 class Config(object):
@@ -15,13 +17,17 @@ class Config(object):
         self.test_path = dataset + '/data/test.txt'                                  # 测试集
         self.class_list = [x.strip() for x in open(
             dataset + '/data/class.txt').readlines()]                                # 类别名单
+        try:
+            os.mkdir(dataset + '/saved_dict/')
+        except:
+            pass
         self.save_path = dataset + '/saved_dict/' + self.model_name + '.ckpt'        # 模型训练结果
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   # 设备
 
         self.require_improvement = 1000                                 # 若超过1000batch效果还没提升，则提前结束训练
         self.num_classes = len(self.class_list)                         # 类别数
         self.num_epochs = 3                                             # epoch数
-        self.batch_size = 128                                           # mini-batch大小
+        self.batch_size = 64                                           # mini-batch大小
         self.pad_size = 32                                              # 每句话处理成的长度(短填长切)
         self.learning_rate = 5e-5                                       # 学习率
         self.bert_path = './bert_pretrain'
